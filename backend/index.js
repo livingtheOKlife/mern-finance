4672cfea2fbf80215a240c7d48d940bed9aebdce
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-05-14 16:55:22 +0100

    backend - database configuration

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import morgan from 'morgan'
 
 dotenv.config()
 
+const PORT = process.env.PORT || 8000
+
 const app = express()
 
 app.use(express.json())
@@ -17,3 +19,10 @@ app.use(morgan('common'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+  })
+  .catch((error) => console.log(`${error}`))
